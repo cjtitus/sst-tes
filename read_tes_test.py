8b00f22db085a6136a0d7ffb1be2dad17606c150
Jamie Titus <ctitus@stanford.edu>
2021-06-07 10:09:50 -0400

    Added testing script

---
+++ read_tes_test.py
@@ -0,0 +1,29 @@
+from bluesky import RunEngine
+from bluesky.plans import scan, count
+import bluesky.preprocessors as bpp
+import bluesky.plan_stubs as bps
+from bluesky.callbacks import LiveTable
+from ophyd.sim import det, motor
+from databroker import Broker
+
+from readable_tes import TES
+
+RE = RunEngine({})
+db = Broker.named('temp')
+RE.subscribe(db.insert)
+
+tes = TES('tes', "", 4000)
+
+def TESPreprocessor(tes):
+    def _log_tes_wrapper(plan):
+        yield from bps.subscribe("start", tes.start_log)
+        return (yield from plan)
+    return _log_tes_wrapper
+
+tpp = TESPreprocessor(tes)
+
+RE.preprocessors = [tpp]
+
+tes._file_start()
+tes.verbose = True
+RE(scan([det, tes], motor, -5, 5, 30))
